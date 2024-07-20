299e2bfb2c17f8fd2937f9143b11100296005c1f
stasrusnak <stasrusnak@gmail.com>
2024-07-20 22:36:50 +0300

    add class

--- src/views/ScrollSpy.vue
+++ src/views/ScrollSpy.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="heading-1">Пример ScrollSpy</h1>
+  <h1 class="heading-1 head_one">Пример ScrollSpy</h1>
   <h1 class="heading-1">Пример ScrollSpy</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, asperiores assumenda commodi cupiditate deleniti
     dolor dolorem eligendi ex laudantium nam nemo obcaecati odio possimus quidem recusandae repellat sapiente vel
