57d2be39ca715da00e922375f30b0934bcf2492d
stasrusnak <stasrusnak@gmail.com>
2024-07-23 18:31:44 +0300

    add new rectangle

--- src/views/ScrollSpy.vue
+++ src/views/ScrollSpy.vue
@@ -1,20 +1,26 @@
 <template>
-  <div>
-    <nav>
-      <ul>
-        <li v-for="section in sections" :key="section.id">
-          <a :href="`#${section.id}`" :class="{ active: activeSection === section.id }">
-            {{ section.name }}
-          </a>
-        </li>
-      </ul>
-    </nav>
-    <div ref="scrollContainer" class="scroll-container">
-      <section v-for="section in sections" :key="section.id" :id="section.id">
+  <div class="scroll_content">
+
+    <div ref="scrollContainer" class="scroll_container">
+      <section v-for="section in sections" :key="section.id" :id="section.id" class="content_section">
         <h2>{{ section.name }}</h2>
         <p>{{ section.content }}</p>
       </section>
     </div>
+
+    <nav class="nav_body">
+      <ul>
+        <li v-for="section in sections" :key="section.id">
+          <div class="nav_section">
+            <div class="rounded_rectangle" v-if="activeSection === section.id"></div>
+            <a :href="`#${section.id}`" :class="{ active: activeSection === section.id }">
+              {{ section.name }}
+            </a>
+          </div> 
+        </li>
+      </ul>
+    </nav>
+
   </div>
 
 </template>
@@ -22,51 +28,70 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from 'vue';
 
-const loerm1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa fugiat iste mollitia ea nobis debitis, assumenda, nisi labore repudiandae praesentium aliquid modi ratione aperiam ipsa tenetur minus aliquam magni amet.'+
-'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem neque perspiciatis magnam doloribus! Obcaecati ipsam accusantium aperiam possimus consequuntur corrupti itaque, nesciunt totam magni tenetur dolore fugiat minus ab velit.'+
-'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis cum amet perspiciatis ad laborum! Atque nemo aspernatur nihil dolorem ab, totam pariatur aut, distinctio provident tenetur repellat sint odio quaerat'+
-'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ratione ad ducimus sed natus nobis consequuntur blanditiis totam pariatur quo expedita, tenetur nam a iure, quisquam quas neque fugit veniam.'
- 
-    const sections = [
-      { id: 'section1', name: 'Section 1', content: loerm1 },
-      { id: 'section2', name: 'Section 2', content: loerm1 },
-      { id: 'section3', name: 'Section 3', content:loerm1 },
-    ];
+const loerm1 = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa fugiat iste mollitia ea nobis debitis, assumenda, nisi labore repudiandae praesentium aliquid modi ratione aperiam ipsa tenetur minus aliquam magni amet.' +
+  'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem neque perspiciatis magnam doloribus! Obcaecati ipsam accusantium aperiam possimus consequuntur corrupti itaque, nesciunt totam magni tenetur dolore fugiat minus ab velit.' +
+  'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis cum amet perspiciatis ad laborum! Atque nemo aspernatur nihil dolorem ab, totam pariatur aut, distinctio provident tenetur repellat sint odio quaerat' +
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, ratione ad ducimus sed natus nobis consequuntur blanditiis totam pariatur quo expedita, tenetur nam a iure, quisquam quas neque fugit veniam.'
 
-    const activeSection = ref(null);
-    const scrollContainer = ref(null);
+const sections = [
+  { id: 'section1', name: 'Section 1', content: loerm1 },
+  { id: 'section2', name: 'Section 2', content: loerm1 },
+  { id: 'section3', name: 'Section 3', content: loerm1 },
+];
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          activeSection.value = entry.target.id;
-        }
-      });
-    }, {
-      root: scrollContainer.value,
-      threshold: 0.5, // adjust threshold as needed
-    });
+const activeSection = ref(null);
+const scrollContainer = ref(null);
 
-    onMounted(() => {
-      sections.forEach(section => {
-        const element = document.getElementById(section.id);
-        if (element) {
-          observer.observe(element);
-        }
-      });
-    });
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      activeSection.value = entry.target.id;
+    }
+  });
+}, {
+  root: scrollContainer.value,
+  threshold: 0.5, // adjust threshold as needed
+});
 
-    onUnmounted(() => {
-      observer.disconnect();
-    });
- 
- 
+onMounted(() => {
+  sections.forEach(section => {
+    const element = document.getElementById(section.id);
+    if (element) {
+      observer.observe(element);
+    }
+  });
+});
+
+onUnmounted(() => {
+  observer.disconnect();
+});
+
+
 </script>
 
-<style>
-.scroll-container {
-  height: 400px;
-  overflow-y: auto;
+<style type="scss" scoped>
+.scroll_content {
+  display: flex;
+
+}
+
+.content_section {
+  height: 500px;
+}
+
+.nav_body {
+  position: sticky;
+  display: block;
+  top: 100px;
+  height: 100%;
+  text-wrap: nowrap;
+
+
+}
+
+.scroll_container {
+  /*height: 400px;
+  overflow-y: auto;*/
 }
 
 nav ul {
@@ -78,6 +103,18 @@ nav li a {
   text-decoration: none;
   padding: 10px;
   display: block;
+  text-wrap: nowrap;
+  content: "";
+
+}
+.nav_section{
+  display: flex;
+}
+.rounded_rectangle {
+  width: 3px;
+  height: 20px;
+  background-color: #4CAF50; 
+  border-radius: 2px;
 }
 
 nav li a.active {
@@ -85,4 +122,3 @@ nav li a.active {
   color: red;
 }
 </style>
-
