a387e9d3cdd5ffc363ae74df2cde6157b82be1a6
stasrusnak <stasrusnak@gmail.com>
2024-10-11 19:54:37 +0300

    add checkbox colors

--- src/components/SimpleUiCheckbox.vue
+++ src/components/SimpleUiCheckbox.vue
@@ -17,6 +17,10 @@ const props = defineProps({
     type: Boolean,
     default: false
   },
+  color: {
+    type: String,
+    default: '',
+  },
   disabled: {
     type: Boolean,
     default: false
@@ -27,83 +31,35 @@ const props = defineProps({
   }
 })
 const handleClock = (e) => {
-  if (props.group) emits('handleCheckGroup',{id:e.target.id, check:e.target.checked})
+  if (props.group) emits('handleCheckGroup', {id: e.target.id, check: e.target.checked})
   else emits('update:checked', e.target.checked)
 }
 </script>
 
 
 <template>
-<div class="checkbox-container">
-  <input
-      class="checkbox"
-      type="checkbox"
-      :id="id"
-      :value="value"
-      :checked="checked"
-      :disabled="disabled"
-      @input="handleClock($event)"/>
-  <label :for="id">{{ label }}</label>
-</div>
+  <div class="checkbox-container">
+    <input
+        :class="[color ? `checkbox checkbox_${color}`:'checkbox']"
+        type="checkbox"
+        :id="id"
+        :value="value"
+        :checked="checked"
+        :disabled="disabled"
+        @input="handleClock($event)"/>
+    <label :for="id">{{ label }}</label>
+  </div>
 
 
 </template>
 
 <style lang="scss" scoped>
-.checkbox-container{
+@import "../styles/checkboxStyles";
+.checkbox-container {
   padding-bottom: 3px;
 }
-.checkbox {
-  position: absolute;
-  z-index: -1;
-  opacity: 0;
-  & + label {
-    display: inline-flex;
-    align-items: center;
-    user-select: none;
-  }
 
-  & + label::before {
-    content: '';
-    display: inline-block;
-    width: 24px;
-    height: 24px;
-    flex-shrink: 0;
-    flex-grow: 0;
-    border: 1px solid #adb5bd;
-    border-radius: 6px;
-    margin-right: 10px;
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 50% 50%;
-  }
 
-  &:checked + label::before {
-    border-color: var(--primary);
-    background-color: var(--primary);
-    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
-  }
 
-  &:not(:disabled):not(:checked) + label:hover::before {
-    border-color: var(--primary-hover);
-  }
 
-  &:not(:disabled):active + label::before {
-    background-color: var(--primary);
-    border: 1px solid #ECEBED;
-  }
-
-  &:focus + label::before {
-    box-shadow: 0px 7px 20px rgba(0, 0, 0, 0.07);
-  }
-
-  &:focus:not(:checked) + label::before {
-    border-color: var(--primary);
-  }
-
-  &:disabled + label::before {
-    background-color: #80848f;
-    border: 1px solid #80848f;
-  }
-}
 </style>
--- src/views/Checkbox.vue
+++ src/views/Checkbox.vue
@@ -41,6 +41,7 @@ const unitSelected = ref(['h001', 'h02p'])
 
     <div class="line checkbox">
       <SimpleUiCheckbox
+          color="success"
           label="Checkbox Active"
           id="checkBoxActive"
           v-model:checked="checkBoxActive">
@@ -102,98 +103,15 @@ const unitSelected = ref(['h001', 'h02p'])
   </div>
 
 
-  <div class="contentBlock">
-    <h2>Emphasis</h2>
-    <div class="description">
-      <p>A button can be formatted to show different levels of emphasis</p>
-      <div class="code_button">
-        <SimpleUiButton buttonText="View code" color="minimal_dark" @click="isEmphasisShow = !isEmphasisShow">
-          <SimpleUiIcon icon="code" size="tiny"></SimpleUiIcon>
-        </SimpleUiButton>
-      </div>
-    </div>
-
-
-    <div class="line">
-      <SimpleUiButton buttonText="Basic" color="basic"></SimpleUiButton>
-      <SimpleUiButton buttonText="Minimal dark" color="minimal_dark"></SimpleUiButton>
-      <SimpleUiButton buttonText="Minimal" color="minimal"></SimpleUiButton>
-      <SimpleUiButton buttonText="Primary" color="primary"></SimpleUiButton>
-      <SimpleUiButton buttonText="Secondary" color="secondary"></SimpleUiButton>
-      <SimpleUiButton buttonText="Success" color="success"></SimpleUiButton>
-      <SimpleUiButton buttonText="Info" color="info"></SimpleUiButton>
-      <SimpleUiButton buttonText="Warning" color="warning"></SimpleUiButton>
-      <SimpleUiButton buttonText="Danger" color="danger"></SimpleUiButton>
-
-    </div>
-    <transition name="fade">
-      <SimpleUiCodeBlock :code="emphasisExample" v-show="isEmphasisShow"></SimpleUiCodeBlock>
-    </transition>
-  </div>
-
-  <div class="contentBlock">
-    <h2>Animations</h2>
-    <div class="description">
-      <p>A button can be formatted to show different levels of emphasis</p>
-      <div class="code_button">
-        <SimpleUiButton buttonText="View code" color="minimal_dark" @click="isAnimationsShow = !isAnimationsShow">
-          <SimpleUiIcon icon="code" size="tiny"></SimpleUiIcon>
-        </SimpleUiButton>
-      </div>
-    </div>
-    <div class="line content_animation">
-      <SimpleUiButton buttonText="Swipe" color="success" animation="swipe"></SimpleUiButton>
-      <SimpleUiButton buttonText="Swipe up" color="success" animation="swipe_up"></SimpleUiButton>
-      <SimpleUiButton buttonText="Swipe down" color="success" animation="swipe_down"></SimpleUiButton>
-      <SimpleUiButton buttonText="Shake" color="primary" animation="shake"></SimpleUiButton>
-      <SimpleUiButton buttonText="Jump" color="secondary" animation="jump"></SimpleUiButton>
-      <SimpleUiButton buttonText="Spacing" color="success" animation="spacing"></SimpleUiButton>
-      <SimpleUiButton buttonText="Shadow hover" color="secondary" animation="shadow_hover"></SimpleUiButton>
-      <SimpleUiButton buttonText="Scale" color="success" animation="scale"></SimpleUiButton>
-      <SimpleUiButton buttonText="Shadow pulse" color="info" animation="shadowPulse"></SimpleUiButton>
-      <SimpleUiButton buttonText="Slide in" color="warning" animation="slideIn"></SimpleUiButton>
-      <SimpleUiButton buttonText="Flicker" color="danger" animation="flicker"></SimpleUiButton>
-      <SimpleUiButton buttonText="Distort" color="info" animation="distort"></SimpleUiButton>
-    </div>
-    <transition name="fade">
-      <SimpleUiCodeBlock :code="animationExample" v-show="isAnimationsShow"></SimpleUiCodeBlock>
-    </transition>
-  </div>
-
-  <div class="contentBlock">
-
-    <h2>Rounded style</h2>
-    <p>A button appearance</p>
-    <div class="line">
-      <SimpleUiButton rounded buttonText="New Button" color="primary"></SimpleUiButton>
-      <SimpleUiButton rounded buttonText="New Button" color="secondary"></SimpleUiButton>
-      <SimpleUiButton rounded buttonText="New Button" color="success"></SimpleUiButton>
-    </div>
-
-    <SimpleUiCodeBlock code='
-  <SimpleUiButton
-    rounded
-    buttonText="New Button"
-    color="success"
-    >
-  </SimpleUiButton>
-'
-                       theme="dark"
-    ></SimpleUiCodeBlock>
-  </div>
-
-
-  <i class="icon-home icon-home__white"></i>
-
 
 </template>
 
 
 <style scoped lang="scss">
+
 .checkbox {
   flex-direction: column;
 }
-
 
 .fade-enter-active,
 .fade-leave-active {
