6c0152f163fed77dcbabc13b5b347b9f16590122
stasrusnak <stasrusnak@gmail.com>
2025-01-13 11:10:10 +0200

    Build Site preview

--- vite.config.js
+++ vite.config.js
@@ -15,27 +15,33 @@ export default defineConfig({
 
   /* Build For library UI */
 
-  build: {
-    lib:{
-      entry: resolve(__dirname, "src/main.js"),
-      name:"simpleui",
-      fileName: (format)=> `simpleui.${format}.js`
-    },
-    rollupOptions:{
-      external:["vue"],
-      output:{
-        globals:{
-          vue:"Vue"
-        }
-      }
-    }
-  },
-  /* Build Site preview */
-
   // build: {
-  //   outDir: 'dist', // Указываем папку, куда будет собрано приложение
-  //   emptyOutDir: true, // Очищаем outDir перед каждой сборкой
-  //   sourcemap: true, // Генерируем sourcemaps для упрощения отладки
+  //   lib:{
+  //     entry: resolve(__dirname, "src/main.js"),
+  //     name:"simpleui",
+  //     fileName: (format)=> `simpleui.${format}.js`
+  //   },
+  //   rollupOptions:{
+  //     external:["vue"],
+  //     output:{
+  //       globals:{
+  //         vue:"Vue"
+  //       }
+  //     }
+  //   }
   // },
 
+
+  /* Build Site preview */
+
+  build: {
+    outDir: 'dist', // Указываем папку, куда будет собрано приложение
+    emptyOutDir: true, // Очищаем outDir перед каждой сборкой
+    sourcemap: true, // Генерируем sourcemaps для упрощения отладки
+  },
+
+
 });
+
+
+
